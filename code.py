946510d9cd85a05a21c84eb3fd6f7406e60cc10b
akhilprabhu20 <akhilprabhu20@gmail.com>
2024-01-24 17:49:56 +0530

    3 files

---
+++ code.py
@@ -0,0 +1,22 @@
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    return x / y
+
+
+x = 10
+y = 15
+print(f"{x} + {y} = {add(x, y)}")
+print(f"{x} - {y} = {subtract(x, y)}")
+print(f"{x} * {y} = {multiply(x, y)}")
+print(f"{x} / {y} = {divide(x, y)}")
